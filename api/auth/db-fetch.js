c9ee1ad335120f742ae9aecd92857b683181b9a8
thibaut.sabot <thibaut.sabot@inovia-team.com>
2017-01-09 11:48:05 +0100

    using apiService for login

--- api/auth/db-fetch.js
+++ api/auth/db-fetch.js
@@ -1,36 +1,17 @@
-var pg = require('pg');
+const ApiService = require('../lib/ApiService.js').ApiService;
 
 module.exports = (req, res, next)=>{
     // only trigger when auth infos is present
     if (!req.auth) return next();
 
-    // instantiate a new client
-    // the client will read connection information from
-    // the same environment variables used by postgres cli tools
-    var client = new pg.Client();
-
-    // connect to our database
-    client.connect(err=>{
-        if (err) throw err;
-
-        // create or retrieve the current user
-        client.query('SELECT * from give_me_time_private.person_register_or_retrieve($1, $2)', [req.auth.fullname, req.auth.email], (err, result)=>{
-            if (err) throw err;
-
-            // this query always return a user
-            req.user = result.rows[0];
-
-            // also refresh credits
-            client.query('SELECT give_me_time_public.everybody_gets_credits()', [], err => {
-                if (err) throw err;
-
-                // disconnect the client
-                client.end(err=>{
-                    if (err) throw err;
+    ApiService('SELECT * from give_me_time_private.person_register_or_retrieve($1, $2)',
+        [req.auth.fullname, req.auth.email], next,
+        result => {
+            req.user = result;
+            ApiService('SELECT give_me_time_public.everybody_gets_credits()',
+                [], next,
+                () => {
+                    next();
                 });
-
-                next();
-            });
         });
-    });
 };
