00032fcc1624d25ab3fa40860443f73d0588f8f4
thibaut.sabot <thibaut.sabot@inovia-team.com>
2017-01-10 10:56:09 +0100

    wrapper function sql migration

--- api/migration/20160609192142-person-table.js
+++ api/migration/20160609192142-person-table.js
@@ -1,26 +1,12 @@
-var fs = require('fs');
+var runSqlFile = require('./helpers/runSqlFile.js');
 var path = require('path');
 
 exports.up = function (db, callback) {
     var filePath = path.join(__dirname + '/sqls/20160609192142-person-table-up.sql');
-    fs.readFile(filePath, { encoding: 'utf-8' }, function (err,data) {
-        if (err) return callback(err);
-        console.log('received data: ' + data);
-        db.runSql(data, function (err) {
-            if (err) return callback(err);
-            callback();
-        });
-    });
+    return runSqlFile(db, filePath, callback);
 };
 
 exports.down = function (db, callback) {
     var filePath = path.join(__dirname + '/sqls/20160609192142-person-table-down.sql');
-    fs.readFile(filePath, { encoding: 'utf-8' }, function (err,data) {
-        if (err) return callback(err);
-        console.log('received data: ' + data);
-        db.runSql(data, function (err) {
-            if (err) return callback(err);
-            callback();
-        });
-    });
+    return runSqlFile(db, filePath, callback);
 };
