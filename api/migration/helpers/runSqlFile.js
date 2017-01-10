a8c2c57b949d7cae7ea556874b0947d8fe18b642
thibaut.sabot <thibaut.sabot@inovia-team.com>
2017-01-10 11:09:38 +0100

    rm logs

--- api/migration/helpers/runSqlFile.js
+++ api/migration/helpers/runSqlFile.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 
 module.exports = function (db, filePath, cb) {
-    console.error("DEOZKDEZOKDZOKDEOZDKEEKODKDDKOEO")
     fs.readFile(filePath, { encoding: 'utf-8' }, function (err,data) {
         if (err) return cb(err);
         console.log('received data: ' + data);
