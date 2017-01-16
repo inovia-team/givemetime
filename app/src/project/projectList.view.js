ee799b8bb1e28a68a0a4bf17e0ed163239638c11
thibaut.sabot <thibaut.sabot@inovia-team.com>
2017-01-16 14:00:13 +0100

    filter project by name

--- app/src/project/projectList.view.js
+++ app/src/project/projectList.view.js
@@ -1,28 +1,48 @@
 import React, { PropTypes } from 'react'
 import ProjectRow from './components/projectRow/projectRow'
 import ProjectPropTypes from './project.propTypes'
+import TextField from 'material-ui/TextField'
 import { List } from 'material-ui/List'
 
 export class ProjectListComponent extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            filter: '',
+        }
+    }
+
+    handleChangeText (x, event) {
+        this.setState({ filter: event })
+    }
 
     componentDidMount () {
-        this.props.loadProjects()
+        if (!this.props.fromProfile)
+            this.props.loadProjects()
     }
     render () {
         return (
-            <List
-              >
-                 {this.props.projects.map((project, i) =>
-                    <div key={i}>
-                        <ProjectRow project={project} />
-                    </div>
-                )}
-            </List>
+            <div>
+                <TextField
+                  hintText="Filter projects by name"
+                  onChange={(x, event) => this.handleChangeText(x,event)}
+                  fullWidth={true}
+                />
+                <List
+                  >
+                     { this.props.projects.length && this.props.projects.filter(project => project.title.toLowerCase().includes(this.state.filter.toLowerCase())).map((project, i) =>
+                        <div key={i}>
+                            <ProjectRow project={project} />
+                        </div>
+                    )}
+                </List>
+            </div>
         )
     }
 }
 
 ProjectListComponent.propTypes = {
-    projects: PropTypes.arrayOf(ProjectPropTypes.isRequired).isRequired,
-    loadProjects: PropTypes.func.isRequired,
+    projects: PropTypes.arrayOf(ProjectPropTypes).isRequired,
+    loadProjects: PropTypes.func,
+    fromProfile: PropTypes.bool,
 }
