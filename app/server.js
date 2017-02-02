6fb0ae2260f8310d4b915e4a55c131a9be5bdd87
julien.desbois <julien.desbois@inovia-team.com>
2017-02-02 17:02:31 +0100

    [TECH] refs #79754 fix app/api server prod listeners

--- app/server.js
+++ app/server.js
@@ -1,13 +1,18 @@
-const express = require('express')
-import { PORT } from './src/config'
-const app = express()
+const express = require('express');
+const errorMiddleware = require('./src/config').errorMiddleware;
+const PORT = require('./src/config').PORT;
+const app = express();
 
 // static file serve
 
-app.use(express.static(__dirname + '/build'))
+app.use(express.static('build'));
+app.use(express.static('assets'));
 // not found in static files, so default to index.html
-app.use((req, res) => res.sendFile(`${__dirname}/build/index.html`))
+app.use(function (req, res) { return res.sendFile(__dirname + '/build/index.html') });
+app.use(errorMiddleware);
 
 // eslint-disable-next-line no-console
-console.log(`Listening to port ${PORT}`)
-app.listen(PORT)
+console.log('Listening to port ' + PORT);
+app.listen(PORT);
+
+module.exports = app;
--- app/src/config.js
+++ app/src/config.js
@@ -1,3 +1,11 @@
-export const PORT = process.env.PORT || 8080
-export const API_URL = (process.env.API_URL || 'http://localhost') + ':' + PORT
-export const GOOGLE_KEY = process.env.GOOGLE_CLIENT_ID
+const PORT = process.env.PORT || 8080;
+module.exports.PORT = PORT;
+module.exports.API_URL = (process.env.API_URL || 'http://localhost') + ':' + PORT;
+module.exports.GOOGLE_KEY = process.env.GOOGLE_CLIENT_ID
+
+// eslint-disable-next-line
+module.exports.errorMiddleware = function (err, req, res, next) {
+    log.error('ERROR : ', err);
+    res.status(err.status || 500);
+    res.json({ error: err });
+};
