58e4e9dc853aff28d3a71b70e88dffab9c78372c
julien.desbois <julien.desbois@inovia-team.com>
2017-02-03 12:07:28 +0100

    [TECH] refs #79754 lints

--- app/server.js
+++ app/server.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const errorMiddleware = require('./src/config').errorMiddleware;
-const PORT = require('./src/config').PORT;
-const app = express();
+const express = require('express')
+const PORT = require('./src/config').PORT
+const app = express()
 
 // static file serve
 
-app.use(express.static('build'));
-app.use(express.static('assets'));
+app.use(express.static('build'))
+app.use(express.static('assets'))
 // not found in static files, so default to index.html
-app.use(function (req, res) { return res.sendFile(__dirname + '/build/index.html') });
-app.use(errorMiddleware);
+app.use(function (req, res) { return res.sendFile(__dirname + '/build/index.html') })
 
 // eslint-disable-next-line no-console
-console.log('Listening to port ' + PORT);
-app.listen(PORT);
+console.log('Listening to port ' + PORT)
+app.listen(PORT)
 
-module.exports = app;
+module.exports = app
--- app/src/config.js
+++ app/src/config.js
@@ -1,15 +1,8 @@
-const PORT = process.env.PORT || 8080;
-const API_PORT = process.env.API_PORT || 3000;
-module.exports.PORT = PORT;
-module.exports.API_PORT = API_PORT;
-module.exports.API_URL = (process.env.API_URL || 'http://localhost') + ':' + API_PORT;
-module.exports.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-module.exports.GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-module.exports.GOOGLE_REDIRECT_URL = process.env.GOOGLE_REDIRECT_URL;
-
-// eslint-disable-next-line
-module.exports.errorMiddleware = function (err, req, res, next) {
-    log.error('ERROR : ', err);
-    res.status(err.status || 500);
-    res.json({ error: err });
-};
+const PORT = process.env.PORT || 8080
+const API_PORT = process.env.API_PORT || 3000
+module.exports.PORT = PORT
+module.exports.API_PORT = API_PORT
+module.exports.API_URL = (process.env.API_URL || 'http://localhost') + ':' + API_PORT
+module.exports.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+module.exports.GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+module.exports.GOOGLE_REDIRECT_URL = process.env.GOOGLE_REDIRECT_URL
