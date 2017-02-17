459145f6e4a806457bdf84e6f7f0dcde5189260f
julien.desbois <julien.desbois@inovia-team.com>
2017-02-17 15:24:41 +0100

    Fix after JC comment

--- app/server.js
+++ app/server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const PORT = require('./src/config').PORT
 const app = express()
+const path = require('path')
 
 // static file serve
 
 app.use(express.static('build'))
 app.use(express.static('assets'))
 // not found in static files, so default to index.html
-app.use(function (req, res) { return res.sendFile(__dirname + '/build/index.html') })
+app.use(function (req, res) { return res.sendFile(path.join(__dirname, '/build/index.html')) })
 
 // eslint-disable-next-line no-console
 console.log('Listening to port ' + PORT)
